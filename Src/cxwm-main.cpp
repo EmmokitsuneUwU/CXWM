b5217373eac052644abe62425c78efc65ddc6bd1
EmmokitsuneUwU <emmokitsune@gmail.com>
2024-12-20 14:55:58 -0300

    code upgrades,maxime keybind and soft-close

--- Src/cxwm-main.cpp
+++ Src/cxwm-main.cpp
@@ -10,18 +10,20 @@
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-int main(void)
+//stupid line for config variables
+std::string configAppLauncher,configAppsPath,configTerminal,
+    configCloseWindow,configExitWM,configAppLauncherKb,configTerminalKb;
+
+int backgroundR = 0,backgroundG = 0,backgroundB = 0;
+
+const char* userHome = getenv("HOME");
+std::string configPath = std::string(userHome) + "/.config/CXWM";
+std::string autostartPath = configPath + "/Autostart.sh";
+    
+std::string appLauncher = configAppLauncher;
+
+void loadSettings()
 {
-    //stupid line for config variables
-    std::string configAppLauncher,configAppsPath,configTerminal,
-        configCloseWindow,configExitWM,configAppLauncherKb,configTerminalKb;
-
-    int backgroundR = 0,backgroundG = 0,backgroundB = 0;
-
-    const char* userHome = getenv("HOME");
-    std::string configPath = std::string(userHome) + "/.config/CXWM";
-    std::string autostartPath = configPath + "/Autostart.sh";
-
     // read settings and stupid things from config.ini
     inipp::Ini<char> ini;
     std::ifstream is(configPath + "/config.ini");
@@ -41,8 +43,39 @@ int main(void)
     inipp::get_value(ini.sections["colors"],"backgroundR",backgroundR);
     inipp::get_value(ini.sections["colors"],"backgroundG",backgroundG);
     inipp::get_value(ini.sections["colors"],"backgroundB",backgroundB);
+}
+void grabKeys(Display *dpy)
+{
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask,
+        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(configCloseWindow.c_str())), Mod1Mask,
+        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(configExitWM.c_str())), Mod1Mask | ShiftMask,
+        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(configAppLauncherKb.c_str())), Mod1Mask,
+        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("Return") ), Mod1Mask,
+        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("R") ), Mod1Mask,
+        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("m") ), Mod1Mask,
+        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
     
-    std::string appLauncher = configAppLauncher;
+    //mouse buttons,why do most of the popular WMs need a mouse,dude
+    XGrabButton(dpy, 1, Mod1Mask, DefaultRootWindow(dpy), True,
+                ButtonPressMask | ButtonReleaseMask | PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+    XGrabButton(dpy, 3, Mod1Mask, DefaultRootWindow(dpy), True,
+                ButtonPressMask | ButtonReleaseMask | PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+}
+
+int main(void)
+{
+    loadSettings();
 
     Display *dpy;
     XWindowAttributes attr;
@@ -84,32 +117,11 @@ int main(void)
     std::cout << "CXWM inited! " << DisplayWidth(dpy, currentScreen) << "w "
         << DisplayHeight(dpy, currentScreen) << "h" << std::endl;
 
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(configCloseWindow.c_str())), Mod1Mask,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(configExitWM.c_str())), Mod1Mask | ShiftMask,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(configAppLauncherKb.c_str())), Mod1Mask,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("Return") ), Mod1Mask,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("R") ), Mod1Mask,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+    grabKeys(dpy);
     
-    //mouse buttons,why do most of the popular WMs need a mouse,dude
-    XGrabButton(dpy, 1, Mod1Mask, DefaultRootWindow(dpy), True,
-                ButtonPressMask | ButtonReleaseMask | PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-    XGrabButton(dpy, 3, Mod1Mask, DefaultRootWindow(dpy), True,
-                ButtonPressMask | ButtonReleaseMask | PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-
     // Low level autostarts
     system((configAppsPath + "CXWMWelcome/CXWMWelcome &").c_str());
+    system((configAppsPath + "CXWMDock/CXWMDock &").c_str());
     // autostart
     int autostartStatus = system(autostartPath.c_str());
 
@@ -135,21 +147,44 @@ int main(void)
         {
             if (ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym(configCloseWindow.c_str())) && (ev.xkey.state & Mod1Mask))
             {
-                if (activeWindow != None)
+                if (activeWindow != None && XGetClassHint(dpy,activeWindow,&classHint))
                 {
-                    std::cout << "killing" << std::endl;
-                    XDestroyWindow(dpy, activeWindow);
-                    activeWindow = None;
-                    ev.xbutton.subwindow = None;
-                    previousWindow = None;
-                    XSetInputFocus(dpy,root,RevertToParent,CurrentTime);
+                    if(strcmp(classHint.res_name,"CXWMDock") != 0)
+                    {
+                        XEvent event;
+                        event.xclient.type = ClientMessage;
+                        event.xclient.window = activeWindow;
+                        event.xclient.message_type = XInternAtom(dpy, "WM_PROTOCOLS", true);
+                        event.xclient.format = 32;
+                        event.xclient.data.l[0] = XInternAtom(dpy, "WM_DELETE_WINDOW", false);
+                        event.xclient.data.l[1] = CurrentTime;
+                        XSendEvent(dpy, activeWindow, False, NoEventMask, &event);
+
+                        activeWindow = None;
+                        ev.xbutton.subwindow = None;
+                        previousWindow = None;
+                        XSetInputFocus(dpy,root,RevertToParent,CurrentTime);
+                    }
+
+                }
+            }
+            if(ev.xkey.keycode == XKeysymToKeycode(dpy,XStringToKeysym("m")) && (ev.xkey.state & Mod1Mask))
+            {
+                if(activeWindow != None && XGetClassHint(dpy,activeWindow,&classHint))
+                {
+                    if(strcmp(classHint.res_name,"CXWMDock") != 0 && strcmp(classHint.res_name,"CXWMWelcome") != 0)
+                    {
+                        XRaiseWindow(dpy,activeWindow);
+                        XMoveResizeWindow(dpy,activeWindow,0,0,DisplayWidth(dpy,currentScreen),DisplayHeight(dpy,currentScreen));
+                    }
+                    if(classHint.res_name) { XFree(classHint.res_name); }
+                    if(classHint.res_class) { XFree(classHint.res_class); }
                 }
             }
 
             if (ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym(configExitWM.c_str())) &&
                 (ev.xkey.state & Mod1Mask) && (ev.xkey.state & ShiftMask))
             {
-                std::cout << "Quit" << std::endl;
                 break;
             }
 
@@ -172,8 +207,6 @@ int main(void)
             if (ev.xbutton.subwindow != None && ev.xbutton.subwindow != backgroundWindow)
             {
                 activeWindow = ev.xbutton.subwindow; //detecting focus change :3
-                std::cout << "active window changed" << std::endl;
-
 
                 XRaiseWindow(dpy,activeWindow);
                 XSetInputFocus(dpy,activeWindow,RevertToParent,CurrentTime);
@@ -190,12 +223,14 @@ int main(void)
             {
                 if(XGetClassHint(dpy,activeWindow,&classHint))
                 {
-                    if(strcmp(classHint.res_name,"") !=0)
+                    if(strcmp(classHint.res_name,"CXWMDock") !=0)
                     {
                         int xdiff = ev.xbutton.x_root - start.x_root;
                         int ydiff = ev.xbutton.y_root - start.y_root;
                         XMoveWindow(dpy,activeWindow,attr.x + (start.button == 1 ? xdiff : 0),attr.y + (start.button == 1 ? ydiff : 0));
                     }
+                    if(classHint.res_name) { XFree(classHint.res_name); }
+                    if(classHint.res_class) { XFree(classHint.res_class); }
                 }
             }
             //Change size
@@ -203,7 +238,7 @@ int main(void)
             {
                 if(XGetClassHint(dpy,activeWindow,&classHint))
                 {
-                    if(strcmp(classHint.res_name,"CXWMWelcome") !=0)
+                    if(strcmp(classHint.res_name,"CXWMWelcome") !=0 && strcmp(classHint.res_name,"CXWMDock") != 0)
                     {
                         int xdiff = ev.xbutton.x_root - start.x_root;
                         int ydiff = ev.xbutton.y_root - start.y_root;
@@ -211,14 +246,16 @@ int main(void)
                             MAX(1, attr.width + (start.button == 3 ? xdiff : 0)),
                             MAX(1, attr.height + (start.button == 3 ? ydiff : 0)));
                     }
+                    if(classHint.res_name) { XFree(classHint.res_name); }
+                    if(classHint.res_class) { XFree(classHint.res_class); }
                 }
             }
         }
+
         else if (ev.type == ButtonRelease)
         {
-            activeWindow = None;
+                activeWindow = None;
         }
-
     }
 
     XCloseDisplay(dpy);
